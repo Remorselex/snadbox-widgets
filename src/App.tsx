826c4d961529a3747bac36d3b1dc05917bfa63c6
Oleg Ostrovskiy <remorselex99@gmail.com>
2023-04-14 17:21:12 +0300

    feat: trying to add style changer

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,11 @@ import { EventId, Widgets } from './utils/interfaces';
 
 function App() {
   const [widgetType, setWidgetType] = useState<Widgets>(Widgets.Agenda);
-  const [EventId, setEventId] = useState<EventId>('');
+  const [eventId, setEventId] = useState<EventId>('15471a89-c3bf-433a-8e8f-068c6d4763f2');
   return (
     <div className='App'>
       <TabsMenu setWidgetType={setWidgetType} setEventId={setEventId} />
-      <WidgetsContainer widgetType={widgetType} eventId={EventId} />
+      <WidgetsContainer widgetType={widgetType} eventId={eventId} />
     </div>
   );
 }
--- src/components/WidgetsContainer/WidgetsContainer.tsx
+++ src/components/WidgetsContainer/WidgetsContainer.tsx
@@ -1,9 +1,12 @@
-import { useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import { WIDGET_SOURCE_URL } from '../../utils/constants';
 import { IWidgetContainerProps } from '../../utils/interfaces';
 
 function WidgetsContainer({ widgetType, eventId }: IWidgetContainerProps) {
+  const widgetsRef = useRef(null);
+  const [bgColor, setBgColor] = useState('#default-color');
+
   useEffect(() => {
     (async () => {
       const script = document.createElement('script');
@@ -11,10 +14,36 @@ function WidgetsContainer({ widgetType, eventId }: IWidgetContainerProps) {
       await document.body.append(script);
     })();
   }, []);
+
+  useEffect(() => {
+    if (widgetsRef.current) {
+      const observer = new MutationObserver(() => {
+        //@ts-ignore
+        const myElement = widgetsRef.current.shadowRoot.querySelector('.agenda-section');
+        console.log(myElement);
+        if (myElement) {
+          myElement.style.setProperty('--agenda-section-bg', bgColor);
+        }
+      });
+
+      observer.observe(widgetsRef.current, { childList: true, subtree: true });
+
+      return () => {
+        observer.disconnect();
+      };
+    }
+  }, []);
+
+  //@ts-ignore
+  const handleColorChange = (e) => {
+    setBgColor(e.target.value);
+  };
+
   return eventId.length === 36 ? (
     <div key={Date.now()}>
-      {/* @ts-ignore*/}
-      <dealroomevent-widgets className='widgetComponent' typeWidget={widgetType} eventId={eventId} />
+      <input type='color' value={bgColor} onChange={handleColorChange} />
+      {/*@ts-ignore*/}
+      <dealroomevent-widgets typeWidget={widgetType} eventId={eventId} ref={widgetsRef} />
     </div>
   ) : (
     <h1 style={{ display: 'flex', justifyContent: 'center', padding: '10% 0' }}>Enter event ID</h1>
