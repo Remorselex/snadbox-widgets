b1348c4aa66dd6c0a484d15d76be24ab37a7c8e2
Oleg Ostrovskiy <remorselex99@gmail.com>
2023-04-26 12:06:35 +0300

    feat: removed unnecessary functionality from react-ace

--- src/components/CssEditor/CssEditor.tsx
+++ src/components/CssEditor/CssEditor.tsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import AceEditor from 'react-ace';
 
+import 'ace-builds/src-noconflict/ace';
 import 'ace-builds/src-noconflict/mode-css';
 import 'ace-builds/src-noconflict/theme-dracula';
 import 'ace-builds/src-noconflict/ext-language_tools';
-import 'ace-builds/webpack-resolver';
-
+import 'ace-builds/src-noconflict/worker-css';
+//import 'ace-builds/webpack-resolver';
 import { IsetWidgetsStyle } from '../../utils/types';
 
 function CssEditor({ setWidgetsStyle }: IsetWidgetsStyle) {
@@ -17,29 +18,31 @@ function CssEditor({ setWidgetsStyle }: IsetWidgetsStyle) {
   };
 
   return (
-    <AceEditor
-      mode='css'
-      theme='dracula'
-      name='css-editor'
-      width='100%'
-      height='auto'
-      placeholder='
-      Here is a field to enter your css code!!!'
-      onChange={handleEditorChange}
-      fontSize={14}
-      showPrintMargin={false}
-      showGutter={true}
-      highlightActiveLine={true}
-      value={editorValue}
-      setOptions={{
-        enableBasicAutocompletion: true,
-        enableLiveAutocompletion: true,
-        enableSnippets: true,
-        showLineNumbers: true,
-        tabSize: 2,
-        splits: 1,
-      }}
-    />
+    <>
+      <AceEditor
+        mode='css'
+        theme='dracula'
+        name='css-editor'
+        width='100%'
+        height='90%'
+        onChange={handleEditorChange}
+        fontSize={14}
+        showPrintMargin={false}
+        showGutter={true}
+        highlightActiveLine={true}
+        value={editorValue}
+        setOptions={{
+          useWorker: false,
+          enableBasicAutocompletion: true,
+          enableLiveAutocompletion: true,
+          enableSnippets: false,
+          showLineNumbers: true,
+          tabSize: 2,
+          splits: 1,
+          wrap: true,
+        }}
+      />
+    </>
   );
 }
 
