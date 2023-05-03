bdd0024278637f889e88007b752812ef6585b5a8
Oleg Ostrovskiy <remorselex99@gmail.com>
2023-05-03 13:43:21 +0300

    feat: added default eventid

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,9 @@ import { EventId, Widgets } from './utils/types';
 function App() {
   const [widgetType, setWidgetType] = useState<Widgets>(Widgets.Agenda);
   const [isEditorOpen, setIsEditorOpen] = useState(true);
-  const [eventId, setEventId] = useState<EventId>(null);
+  const [eventId, setEventId] = useState<EventId>(
+    '8a87a9bc-1382-4e71-b918-1105bddefd08'
+  );
   return (
     <div className='App'>
       <TabsMenu
